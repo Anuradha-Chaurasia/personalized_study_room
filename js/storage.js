f91c1d8d4fd393589f25bf9005302ca37571891e
Anuradha <parichaurasiya014@gmail.com>
2026-07-31 23:59:44 +0530

    Add files via upload

---
+++ js/storage.js
@@ -0,0 +1,1 @@
+window.Store={get(k,f){try{return JSON.parse(localStorage.getItem('haven_'+k))??f}catch{return f}},set(k,v){localStorage.setItem('haven_'+k,JSON.stringify(v))},clear(){Object.keys(localStorage).filter(k=>k.startsWith('haven_')).forEach(k=>localStorage.removeItem(k))}};
